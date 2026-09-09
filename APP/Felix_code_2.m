c210b2aae2f7f7ed50126b29a0c9099dd66d5a75
skid0u <115102269+skid0u@users.noreply.github.com>
2026-09-09 14:33:51 -0400

    gossage lpc
    pppppp

---
+++ APP/Felix_code_2.m
@@ -0,0 +1,88 @@
+%% Clear and Load
+
+clear
+[sig, Fe] = audioread('hel_fr1.wav');
+sig = sig';
+
+%% Approche LPC
+
+clc
+N          = length(sig);
+L          = 50e-3*Fe;           % Longueur d'une trame (50 ms à Fe = 44.1 kHz)
+LW         = 2 * L;              % Longueur de la fenêtre (chevauchement de 50 %)
+N_trames   = floor(N / L) - 1;   % Nombre de trames
+w          = sqrt(hanning(LW))'; % Fenêtre d'analyse/synthèse COLA
+m          = 20;                 % Ordre LPC (m=10 à 20 recommandé)
+k          = 2;
+
+ptr            = 1;
+mem_synthese   = zeros(1, L);
+mem_lpc_synth  = zeros(1, m);    % Mémoire des états pour le filtre IIR
+trame_analyse  = zeros(1, LW);
+signal_filtre  = zeros(1, N);
+
+% boucle principale
+
+for trame = 1 : N_trames
+
+    % Trames
+    new_frame = sig(ptr : ptr + L - 1);
+    trame_analyse(1 : end/2) = trame_analyse(end/2+1 : end);
+    trame_analyse(end/2+1 : end) = new_frame;
+    
+    % Fenêtrage Hanning
+    xw = trame_analyse .* w;
+    
+    % Coefficients LPC
+    A_LPC = lpc(xw, m);
+    
+    % Erreur
+    err = filter(A_LPC, 1, xw);
+
+    % Modification
+    [h,wf] = freqz(1,A_LPC,L);
+    mag = abs(h);
+    phase = angle(h)
+    w_comp = wf/k;
+    mag_mod = interp1(w_comp, mag, wf);
+    phase_mod = interp1(w_comp, phase, wf);
+
+    H_demi = mag_mod .* exp(1i * phase_mod);
+    H_complet = [H_demi; conj(H_demi(end-1 :-1 : 2))];
+
+    h_temporel = real(ifft(H_complet));
+    h_temporel = h_temporel';
+
+
+    y = filter(h_temporel, 1, err);
+    
+    
+    % Fenêtrage Hanning 2
+    yw = y .* w;
+    
+    % OLA
+    trame_OLA = yw(1 : end/2) + mem_synthese;
+    signal_filtre(ptr : ptr + L - 1) = trame_OLA;   
+    mem_synthese = yw(end/2+1 : end);
+    ptr = ptr + L;
+
+    fig = figure(1);
+    subplot(2,1,1);
+    plot(mag);
+    subplot(2,1,2);
+    plot(mag_mod);
+
+    fig = figure(2);
+    subplot(2,1,1);
+    plot(mag);
+    subplot(2,1,2);
+    plot(mag_mod);
+
+    waitfor(fig);
+end
+
+signal_filtre = signal_filtre / max(abs(signal_filtre)) * max(abs(sig));
+% max(abs(signal_filtre)) * max(abs(sig));
+% signal_filtre = signal_filtre / 40000;
+
+sound(signal_filtre, Fe);
